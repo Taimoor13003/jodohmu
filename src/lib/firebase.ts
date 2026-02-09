6a8a82128108d0824fc35fd886e34227265bafbe
Taimoor Ghori <taimoor@slash.co>
2026-02-09 14:54:45 +0700

    test

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,8 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfigFromEnv = '{"apiKey":"AIzaSyCyrZ6cY1R0sp-5a6Aoz9Mg5XTDBqXQQ5w","authDomain":"jodohmu-production.firebaseapp.com","projectId":"jodohmu-production","storageBucket":"jodohmu-production.appspot.com","messagingSenderId":"460634545907","appId":"1:460634545907:web:fac58c25cb1b352448e186","measurementId":"G-8XHZM9P9F3"}'
+// const firebaseConfigFromEnv = '{"apiKey":"AIzaSyCyrZ6cY1R0sp-5a6Aoz9Mg5XTDBqXQQ5w","authDomain":"jodohmu-production.firebaseapp.com","projectId":"jodohmu-production","storageBucket":"jodohmu-production.appspot.com","messagingSenderId":"460634545907","appId":"1:460634545907:web:fac58c25cb1b352448e186","measurementId":"G-8XHZM9P9F3"}'
+const firebaseConfigFromEnv = '{}'
 
 const firebaseConfig = JSON.parse(firebaseConfigFromEnv) 
 // ?? {
